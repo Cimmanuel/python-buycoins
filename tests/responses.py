886ecd9eb0111c780e4ff05cf49851bac52673cc
Immanuel Kolapo <immanuelcaspian@gmail.com>
2021-02-09 06:35:18 +0100

    chore: add more responses

--- tests/responses.py
+++ tests/responses.py
@@ -1,3 +1,5 @@
+# Query responses
+
 get_balance = {
     "data": {
         "getBalances": [
@@ -185,25 +187,106 @@ get_orders = {
     }
 }
 
-# get_payments = {
-#     "data": {
-#         "getPayments": {
-#             "edges": [
-#                 {
-#                     "node": {
-#                         "id": "UG9zdE9yZGVyLTg5MDM4MzI4LTc5MzItNGUxMS1hZWZjLTkwYjg4ZTFhY2JjOA==",
-#                         "cryptocurrency": "bitcoin",
-#                         "coinAmount": "0.01060841",
-#                         "side": "sell",
-#                         "status": "active",
-#                         "createdAt": 1612789170,
-#                         "pricePerCoin": "20757600.0",
-#                         "priceType": "dynamic",
-#                         "staticPrice": None,
-#                         "dynamicExchangeRate": "480",
-#                     }
-#                 }
-#             ]
-#         }
-#     }
-# }
+get_payments = {
+    "data": {
+        "getPayments": {
+            "edges": [
+                {
+                    "node": {
+                        "id": "UG9zdE9yZGVyLTg5MDM4MzI4LTc5MzItNGUxMS1hZWZjLTkwYjg4ZTFhY2JjOA==",
+                        "fee": "0.0046",
+                        "amount": "10000.00",
+                        "createdAt": 1605000847,
+                        "reference": "38d5d9018bde98e88058746788d72e936d158f5ad753073f4763dc1d4aa5a48e",
+                        "status": "success",
+                        "totalAmount": "10000.004600",
+                        "type": "deposit",
+                    }
+                }
+            ]
+        }
+    }
+}
+
+get_price = {
+    "data": {
+        "getPrices": [
+            {
+                "id": "QnV5Y29pbnNQcmljZS0yOWFmZWY4MS1mZjI5LTQwYTQtYmQ3Zi1iOTgzMTA3NmU5NDg=",
+                "status": "active",
+                "cryptocurrency": "ethereum",
+                "minBuy": "0.02",
+                "minSell": "0.02",
+                "maxBuy": "48.07685652",
+                "maxSell": "0",
+                "minCoinAmount": "0.02",
+                "expiresAt": 1612847332,
+                "buyPricePerCoin": "816107.8759",
+                "sellPricePerCoin": "799786.8945",
+            }
+        ]
+    }
+}
+
+get_prices = {
+    "data": {
+        "getPrices": [
+            {
+                "id": "QnV5Y29pbnNQcmljZS01OTkwYTQ0NC1hYjY4LTQxM2MtODUzZC04OWJhYzRhMWNjZjE=",
+                "status": "active",
+                "cryptocurrency": "bitcoin",
+                "minBuy": "0.001",
+                "minSell": "0.001",
+                "maxBuy": "1.78700697",
+                "maxSell": "1.20119207",
+                "minCoinAmount": "0.001",
+                "expiresAt": 1612847212,
+                "buyPricePerCoin": "21956210.523",
+                "sellPricePerCoin": "21521388.24",
+            },
+            {
+                "id": "QnV5Y29pbnNQcmljZS05MjQwNmQ0Zi00MmJlLTQ2MjEtOTY1Ny1mYTM0OGZkNmI2NDE=",
+                "status": "active",
+                "cryptocurrency": "ethereum",
+                "minBuy": "0.02",
+                "minSell": "0.02",
+                "maxBuy": "48.12180182",
+                "maxSell": "0",
+                "minCoinAmount": "0.02",
+                "expiresAt": 1612847212,
+                "buyPricePerCoin": "815345.6391",
+                "sellPricePerCoin": "799154.344",
+            },
+            {
+                "id": "QnV5Y29pbnNQcmljZS04NDMyZmI2OS1iMzAyLTQ2YzQtYjFjZC1kZTM2MjNhMTFiYmU=",
+                "status": "active",
+                "cryptocurrency": "litecoin",
+                "minBuy": "0.1",
+                "minSell": "0.1",
+                "maxBuy": "489.37725284",
+                "maxSell": "134.73978949",
+                "minCoinAmount": "0.1",
+                "expiresAt": 1612847213,
+                "buyPricePerCoin": "80175.1635",
+                "sellPricePerCoin": "78560.0343",
+            },
+            {
+                "id": "QnV5Y29pbnNQcmljZS05OGI0NTE5ZC05NTJlLTRkNGMtODk1OC03MWI2MjRlZmZlMjc=",
+                "status": "active",
+                "cryptocurrency": "usd_coin",
+                "minBuy": "5",
+                "minSell": "5",
+                "maxBuy": "83903.73",
+                "maxSell": "495437.574734",
+                "minCoinAmount": "5",
+                "expiresAt": 1612847213,
+                "buyPricePerCoin": "467.63",
+                "sellPricePerCoin": "458.3206",
+            },
+        ]
+    }
+}
+
+
+# Mutation responses
+
